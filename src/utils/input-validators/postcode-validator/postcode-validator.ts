9b2bb4176be5e61d336a45834f687613a4491d30
Vahan Minasian <vahan.minasian@powerhouse.nl>
2019-03-24 12:40:08 +0100

    feat(license-plate-input): added license plate input component
    feat #25

--- src/utils/input-validators/postcode-validator/postcode-validator.ts
+++ src/utils/input-validators/postcode-validator/postcode-validator.ts
@@ -2,8 +2,8 @@ import { Validator } from '../validators.interface';
 
 export const PostCodeValidator: Validator<string> = {
   validate: (val: string) => {
-    const dutchPostCoderegex = /^[1-9][0-9]{3} ?(?!sa|sd|ss)[a-z]{2}$/i;
-    return dutchPostCoderegex.test(val);
+    const dutchPostCodeRegex = /^[1-9][0-9]{3} ?(?!sa|sd|ss)[a-z]{2}$/i;
+    return dutchPostCodeRegex.test(val);
   },
-  errorMessage: 'Please enter a valid postcode of format: 1234 aa'
+  errorMessage: 'Please enter a valid postcode (e.g. format: 1234 aa)'
 };
--- src/utils/input-validators/validators.factory.ts
+++ src/utils/input-validators/validators.factory.ts
@@ -2,11 +2,14 @@ import { Validator } from './validators.interface';
 import { ValidatorsTypes } from './validators.types';
 import { PostCodeValidator } from './postcode-validator/postcode-validator';
 import { defaultValidator } from './default-validator/default-validator';
+import { LicensePlateValidator } from './license-plate-validator/license-plate-validator';
 
 export function validatorsFactory(type: string): Validator<any> {
   switch(type) {
-    case (ValidatorsTypes.postcode):
+    case (ValidatorsTypes.postCode):
       return PostCodeValidator;
+    case (ValidatorsTypes.licensePlate):
+      return LicensePlateValidator;
     default:
       return defaultValidator;
   }
